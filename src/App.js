6cd0335f4cea59bfb6c2e2d330aae9113a8f9ba2
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2022-03-16 19:28:06 +0100

    add font awesome fix test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 import Routes from "./routes/Routes";
 import HomePage from "./layouts/HomePage/HomePage";
 import { BrowserRouter } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUser);
 
 function App() {
   return (
