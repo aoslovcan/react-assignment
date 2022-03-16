d04c5648ed1e84ba2c924e73fbf5c4008fe78629
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2022-03-16 19:28:55 +0100

    add card post component and style

--- src/components/card-post/CardPost.js
+++ src/components/card-post/CardPost.js
@@ -1,23 +1,26 @@
 import React from "react";
 import style from "./CardPost.module.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const CardPost = (props) => {
   console.log(`${props.message} CardPost`);
+  console.log(props.authorData[0]?.name);
+  console.log(props.key);
   return (
-    <div className={`col-3 ${style.cardPost}`}>
-      <div className={`${style.postAuthorName}`}>Name Surname</div>
-      <div className={`${style.cardPostTitle}`}>
-        <h3>Lorem ipsum</h3>
-      </div>
-      <div className={`${style.cardPostHeader}`}>
-        <img src="https://picsum.photos/200/300" alt="img" />
-      </div>
-      <div className={`${style.cardPostContent}`}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </div>
+    <div className="col-3">
+      <Link to={`posts/${props.id}`} className={` ${style.cardPost}`}>
+        <div className={`${style.cardPostTitle}`}>
+          <h5>{props.title}</h5>
+        </div>
+        <div className={`${style.postAuthorName}`}>
+          <FontAwesomeIcon icon="user" /> {props.authorData[0]?.name}
+        </div>
+        <div className={`${style.cardPostHeader}`}>
+          <img src={`${props.imgUrl}`} alt="img" />
+        </div>
+        <div className={`${style.cardPostContent}`}>{props.content}</div>
+      </Link>
     </div>
   );
 };
