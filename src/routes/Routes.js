703ceaec095aeeeb51d24d9bdaec5b7b640b6bc4
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2022-03-16 19:31:14 +0100

    add route for single post page

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import HomePage from "../layouts/HomePage/HomePage";
+import PostPage from "../layouts/PostPage/PostPage";
 
 function Routes(props) {
   const helloMessage = "Hello from";
   return (
     <Switch>
-      <Route path="/posts">
+      <Route exact path="/posts">
         <HomePage message={helloMessage} />
       </Route>
-      <Route path="/">
+      <Route exact path="/posts/:id">
+        <PostPage message={helloMessage} />
+      </Route>
+      <Route exact path="/">
         <Redirect to="/posts" />
       </Route>
     </Switch>
   );
 }
+
 export default Routes;
