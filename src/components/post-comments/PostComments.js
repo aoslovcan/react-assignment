dfacd8d0b28bafc836847bcfd7bdb3bd488791b1
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2022-03-19 19:50:46 +0100

    post comment component style and unit tests

--- src/components/post-comments/PostComments.js
+++ src/components/post-comments/PostComments.js
@@ -1,7 +1,29 @@
 import React from "react";
+import style from "./PostComments.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PostComments = (props) => {
-  return <div></div>;
+  console.log(`${props?.message} PostComments`);
+
+  return (
+    <div class={`${style.postComments}`} data-testid="post-comments">
+      <div className={`${style.postCommentsAuthor}`}>
+        <FontAwesomeIcon className="user-icon" icon="user" />
+        <span
+          className={`${style.postCommentsEmail}`}
+          data-testid="post-comment-email"
+        >
+          {props?.email}
+        </span>
+      </div>
+      <div className={`${style.postCommentsName}`}>
+        <p data-testid="post-comment-name">{props?.name}</p>
+      </div>
+      <div className={`${style.postCommentsContent}`}>
+        <p data-testid="post-comment-body">{props?.body}</p>
+      </div>
+    </div>
+  );
 };
 
 export default PostComments;
