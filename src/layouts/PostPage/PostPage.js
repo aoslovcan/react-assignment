ad8c4cf7bd674d708303183a7198641646621eae
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2022-03-19 19:52:48 +0100

    implement post data and comments unit tests

--- src/layouts/PostPage/PostPage.js
+++ src/layouts/PostPage/PostPage.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { getPostDetails, getPostComments } from "../../api/allApis";
 import SinglePost from "../../components/single-post/SinglePost";
+import PostComments from "../../components/post-comments/PostComments";
+import style from "./PostPage.module.css";
 import { useParams } from "react-router-dom";
 
 const PostPage = (props) => {
   const { id } = useParams();
   const [postDetail, setPostDetail] = useState();
   const [postComments, setPostComments] = useState([]);
+
   console.log(props?.message + "PostPage");
+
   useEffect(() => {
     getPostDetails(id).then((res) => {
       setPostDetail(res);
@@ -19,9 +23,40 @@ const PostPage = (props) => {
   }, []);
 
   return (
-    <div className="container">
-      {postDetail ? <SinglePost postData={postDetail} /> : <p>Loading...</p>}
-    </div>
+    <>
+      <span className="go-back-button">Go back</span>
+      <div className="container" data-testid="post-page">
+        <div className="row">
+          <div className="col-12">
+            {postDetail ? (
+              <SinglePost
+                imgUrl="https://picsum.photos/1300/250"
+                message={props?.message}
+                postData={postDetail}
+              />
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
+          <div className="col-8 offset-2">
+            <h5>Comments</h5>
+            {postComments
+              ? postComments.map((comment, i) => {
+                  return (
+                    <PostComments
+                      key={i}
+                      name={comment.name}
+                      body={comment.body}
+                      email={comment.email}
+                      message={props?.message}
+                    />
+                  );
+                })
+              : null}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
